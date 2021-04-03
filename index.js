207087af5d5ae24bf27b04fcfe2b8b73d642b86c
sherralotz <sherralotz@gmail.com>
2021-04-03 12:59:03 +0800

    updated smooth scrolling

--- index.js
+++ index.js
@@ -1,82 +1,88 @@
-$(function(){  
-    var animation = bodymovin.loadAnimation({
-      container: document.getElementById('main-anim'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: 'data.json'
-    })
+$(function () {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById("main-anim"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "data.json",
   });
-  
-  
-  (function() {
-    var elements;
-    var windowHeight;
-  
-    function init() {
-      elements = document.querySelectorAll('.hidden');
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
-  
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add('fade-in-element');
-          element.classList.remove('hidden');
-        }
+});
+
+(function () {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".hidden");
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add("fade-in-element");
+        element.classList.remove("hidden");
       }
     }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition(); 
-  })();
+  }
 
-  document.querySelector('.toggle').addEventListener('click', function() {
-    $(this).find(".icon").addClass("animateicon");
-    $(this).find(".icon").toggleClass("fa-sun fa-moon");  
-    $(this).find(".icon").toggleClass("text-white", $(this).find(".icon").hasClass("fa-moon"));  
-    $("body").toggleClass("lightmode darkmode");     
-}); 
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+})();
+
+document.querySelector(".toggle").addEventListener("click", function () {
+  $(this).find(".icon").addClass("animateicon");
+  $(this).find(".icon").toggleClass("fa-sun fa-moon");
+  $(this)
+    .find(".icon")
+    .toggleClass("text-white", $(this).find(".icon").hasClass("fa-moon"));
+  $("body").toggleClass("lightmode darkmode");
+});
 
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 50,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
       }
     }
   });
