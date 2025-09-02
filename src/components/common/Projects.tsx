415e86327a20d40ecec82fd2cf71cc6e8d374109
Sherra Paltingca <sherralotz@gmail.com>
2025-09-01 20:07:45 -0400

    Updated to new username

--- src/components/common/Projects.tsx
+++ src/components/common/Projects.tsx
@@ -12,7 +12,7 @@ const projects = [
     `This project emerged from my own need to simplify travel planning, which typically involved a combination of Sheets and Notes. 
     Using Vue.js, I created a dedicated application to manage all trip details in one place, serving as a practical exercise in the framework.`,
     imageUrl: TravelPlanner,
-    link: "https://sherralotz.github.io/travel-planner/", 
+    link: "https://sherradev.github.io/travel-planner/", 
     skills: [
       { skill: "Vue.js", logo: <Code2 className="w-4 h-4" /> },
       { skill: "JavaScript", logo: <Cpu className="w-4 h-4" /> },
@@ -29,7 +29,7 @@ const projects = [
     Leveraging the Google Books and Open Library APIs, it offers a robust search and book information experience. 
     My favorite feature is that you can export your review so you can share it on social media.`,
     imageUrl: BookTracker,
-    link: "https://sherralotz.github.io/booktracker/", 
+    link: "https://sherradev.github.io/booktracker/", 
     skills: [
       { skill: "React", logo: <Code2 className="w-4 h-4" /> },
       { skill: "JavaScript", logo: <Cpu className="w-4 h-4" /> },
@@ -45,7 +45,7 @@ const projects = [
     description:
       "This web application is a basic implementation of a movie search engine, created to practice React development skills. By connecting to the TMDb API, users can query for movies, and the application dynamically renders the retrieved information. The project likely involved working with React components, managing API responses, and presenting data in a user-friendly format.",
     imageUrl: SearchMovies,
-    link: "https://sherralotz.github.io/search-movies/", 
+    link: "https://sherradev.github.io/search-movies/", 
     skills: [
       { skill: "React", logo: <Code2 className="w-4 h-4" /> },
       { skill: "JavaScript", logo: <Cpu className="w-4 h-4" /> },
